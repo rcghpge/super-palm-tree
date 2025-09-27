3d8ad995d52653d813a5f8cf0fe0feaeae993577
Robert Cocker <rcocker87@gmail.com>
2025-09-27 08:49:06 -0500

    comments are too happy. keep it fonzi

--- rust/raii.rs
+++ rust/raii.rs
@@ -5,7 +5,7 @@ fn create_box() {
     // Allocate an integer on the heap
     let _box1 = Box::new(3i32);
 
-    // `_box1` is destroyed here, and memory gets freed
+    // `_box1` is destroyed here, memory gets freed
 }
 
 fn main() {
@@ -17,14 +17,13 @@ fn main() {
         // Allocate an integer on the heap
         let _box3 = Box::new(4i32);
 
-        // `_box3` is destroyed here, and memory gets freed
+        // `_box3` is destroyed here, memory gets freed
     }
 
-    // Creating lots of boxes just for fun
-    // There's no need to manually free memory!
+    // Generating boxes
     for _ in 0u32..1_000 {
         create_box();
     }
 
-    // `_box2` is destroyed here, and memory gets freed
+    // `_box2` is destroyed here, memory gets freed
 }
